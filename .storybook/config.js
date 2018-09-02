12034c9cb39e41b29121057dce025a94fd8e9e99
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2018-09-02 19:20:23 -0300

    feat: improve the storybook configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,18 @@
 import { configure } from '@storybook/react';
 
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
+
 function loadStories() {
-  require('../stories/index.js');
-  // You can require as many stories as you need.
+  const cache = {};
+
+  function importAll (r) {
+    r.keys().forEach(key => cache[key] = r(key));
+  }
+
+  importAll(require.context('../', true, /\.story\.js$/));
+  // At build-time cache will be populated with all required modules.
 }
 
 configure(loadStories, module);
